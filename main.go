93d4b58336a838d783faa0ade871f10c278b9aaa
Diogo Melim <dmpmelim@gmail.com>
2024-04-12 20:03:38 +0100

    update: added subdomains and method based routing.

--- main.go
+++ main.go
@@ -3,19 +3,40 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/dmelim/go-std-routing/middleware"
 )
 
 func main() {
 	router := http.NewServeMux()
 
 	// Passing paremeters in the url
-	router.HandleFunc("/item/{id}", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("GET /item/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id := r.PathValue("id")
-		w.Write(([]byte("Receives request for items: " + id)))
+		w.Write(([]byte("Received request for item: " + id)))
 	})
+	// Method based routing POST
+	router.HandleFunc("POST /item/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		w.Write(([]byte("We saved item:  " + id)))
+	})
+	// Method based routing DELETE
+	router.HandleFunc("DELETE /item/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		w.Write(([]byte("We deleted item:  " + id)))
+	})
+	// Can Handle host names instead of paths
+	router.HandleFunc("hostname.com/", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		w.Write(([]byte("We deleted item:  " + id)))
+	})
+
+	v1 := http.NewServeMux()
+	v1.Handle("/v1/", http.StripPrefix("/v1", router))
+
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: router,
+		Handler: middleware.Logging(v1),
 	}
 	log.Println("Starting server on port 8080")
 	server.ListenAndServe()
